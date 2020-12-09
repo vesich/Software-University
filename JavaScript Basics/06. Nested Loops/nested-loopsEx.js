f08bcd7ea4cb55ff93e7608bdb79c884b29db36f
Veselin Georgiev <veselin.georgief@gmail.com>
2020-12-09 17:00:50 +0200

    another problem solved

--- JavaScript Basics/06. Nested Loops/nested-loopsEx.js
+++ JavaScript Basics/06. Nested Loops/nested-loopsEx.js
@@ -54,6 +54,48 @@ function solve(inputData) {
 
 //// 03. Sum Prime Non Prime
 
+function solve(inputData) {
+    let index = 0;
+    let currentInfo = inputData[index];
+    let primeSum = 0;
+    let nonPrimeSum = 0;
+
+    while (currentInfo !== "stop") {
+        let number = Number(currentInfo);
+        let lastDivider = Math.sqrt(number);
+        let isPrime = true;
+
+        if (number >= 0) {
+            for (let divider = 2; divider <= lastDivider; divider++) {
+                let result = number % divider;
+                if (result === 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime === true) {
+                primeSum += number;
+            } else {
+                nonPrimeSum += number;
+            }
+        } else {
+            console.log("Number is negative.");
+        }
+        index++;
+        currentInfo = inputData[index];
+    }
+    console.log(`Sum of all prime numbers is: ${primeSum}`);
+    console.log(`Sum of all non prime numbers is: ${nonPrimeSum}`);
+}
+solve([
+    '3', '9',
+    '0', '7',
+    '19', '4',
+    'stop'
+])
+
+
+
 
 //// 04. Train The Trainers
 
