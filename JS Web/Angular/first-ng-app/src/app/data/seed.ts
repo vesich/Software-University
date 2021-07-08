5adf276b94d5fdd8f774b92ce54850168e53cabc
Veselin Georgiev <veselin.georgief@gmail.com>
2021-07-08 15:46:40 -0400

    first touch of angular

---
+++ JS Web/Angular/first-ng-app/src/app/data/seed.ts
@@ -0,0 +1,27 @@
+const data = [{
+    title: 'Article 1',
+    description: 's simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange',
+    author: 'Ivan Vazov',
+    imageUrl: 'http://ichef.bbci.co.uk/news/976/cpsprodpb/10434/production/_90121666_agreementcartoon.jpg'
+},
+{
+    title: 'Article 2',
+    description: 's simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 150ok a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange',
+    author: 'Vazov Ivan',
+    imageUrl: 'http://www.digitalmeetsculture.net/wp-content/uploads/2015/04/article.jpg'
+},
+{
+    title: 'Article 3',
+    description: 's siprinter took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange urvived not only five centuries, but also the leap into electronic typesetting, remaining essenti',
+    author: 'Petkan Draganov',
+    imageUrl: 'http://i.dailymail.co.uk/i/pix/2010/04/17/article-1266852-092DE58A000005DC-112_634x411.jpg'
+},
+{
+    title: 'Article 4',
+    description: 's simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electroge',
+    author: 'Author 4',
+    imageUrl: 'https://www.seoclerk.com/pics/518476-1iDFBR1489198900.png'
+}
+]
+
+export { data };
