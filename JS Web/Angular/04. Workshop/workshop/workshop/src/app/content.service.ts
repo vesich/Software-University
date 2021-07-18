3c29b3f2d9c2d74f6a3a00d994bcb87c5eeb4daa
Veselin Georgiev <veselin.georgief@gmail.com>
2021-07-18 19:40:33 -0400

    setting up environment

--- JS Web/Angular/04. Workshop/workshop/workshop/src/app/content.service.ts
+++ JS Web/Angular/04. Workshop/workshop/workshop/src/app/content.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPost, ITheme } from './shared/interfaces';
+import { environment } from '../environments/environment';
+const API_URL = environment.apiURL;
 
 @Injectable()
 export class ContentService {
@@ -8,11 +10,11 @@ export class ContentService {
   constructor(private http: HttpClient) { }
 
   loadThemes() {
-    return this.http.get<ITheme[]>('http://localhost:3000/api/themes')
+    return this.http.get<ITheme[]>(`${API_URL}/api/themes`)
   }
 
   loadPosts(limit?: number) {
     const query = limit ? `?limit=${limit}` : ''
-    return this.http.get<IPost[]>(`http://localhost:3000/api/posts${query}`)
+    return this.http.get<IPost[]>(`${API_URL}/api/posts${query}`)
   }
 }
